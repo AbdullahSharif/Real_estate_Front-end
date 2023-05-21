08252298f3fc8bca5811883003daeea9852fdb3c
AbdullahSharif <abdullah.29.umar@gmail.com>
2023-05-21 22:10:18 +0500

    hero components completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import {AiOutlineSearch} from "react-icons/ai";
 
 
 
@@ -10,15 +11,15 @@ function Hero() {
   return (
     <div className={`bg-[url('src/assets/bg.webp')] h-screen mx-6 my-10 rounded-xl bg-blend-darken bg-black/[0.5] bg-no-repeat bg-cover flex flex-col items-center justify-center space-y-12`}>
       <h1 className=" text-3xl md:text-6xl text-slate-800 bg-white p-8 shadow-2xl rounded-lg">Let&apos;s find the best for you </h1>
-      <div className="space-x-6">
+      <div className="space-x-6 flex flex-col space-y-9 md:flex-row md:space-y-0 items-center">
 
-        <select name="type" onChange={(e) => setType(e.target.value)}>
+        <select name="type" onChange={(e) => setType(e.target.value)} className="outline-none border-none p-4 text-lg rounded-lg">
           <option disabled>Select Property Type</option>
           <option value="residential">Residential</option>
           <option value="commercial">Commercial</option>
         </select>
 
-        <select name="price" onChange={(e) => setPrice(e.target.value)}>
+        <select name="price" onChange={(e) => setPrice(e.target.value)} className="outline-none border-none p-4 text-lg rounded-lg">
           <option disabled>Select Price Range</option>
           <option value="1,000,000 - 2,500,000">1,000,000 - 2,500,000 Rs</option>
           <option value="2,500,000 - 4,500,000">2,500,000 - 4,500,000 Rs</option>
@@ -29,7 +30,7 @@ function Hero() {
           <option value="10,000,000">Above 10,000,000 Rs</option>
         </select>
 
-        <select name="city" onChange={(e) => setCity(e.target.value)}>
+        <select name="city" onChange={(e) => setCity(e.target.value)} className="outline-none border-none p-4 text-lg rounded-lg">
           <option disabled>Select City</option>
           <option value="all">Anywhere</option>
           <option value="lahore">Lahore</option>
@@ -44,6 +45,7 @@ function Hero() {
           <option value="sargodha">Sargodha</option>        
         </select>
 
+        <button className="bg-white p-2 rounded-lg hover:bg-orange-400 transition ease-linear"><AiOutlineSearch size={25}/></button>
         
 
       </div>
