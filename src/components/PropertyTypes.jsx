a518560a19190db258406f430527aaffc3e11402
AbdullahSharif <abdullah.29.umar@gmail.com>
2023-05-21 23:13:16 +0500

    scrollbar hide functionality added

--- src/components/PropertyTypes.jsx
+++ src/components/PropertyTypes.jsx
@@ -28,7 +28,7 @@ function PropertyTypes() {
       <div className="flex md:flex-row flex-col md:justify-around space-y-6 md:space-y-0">
 
       
-      <Link className="bg-slate-800 p-6 rounded-tr-[4rem] rounded-bl-[4rem] flex flex-col space-y-4 items-center hover:scale-105 transition ease-in-out shadow-2xl">
+      <Link to={"/properties/all/property?type=commercial"} className="bg-slate-800 p-6 rounded-tr-[4rem] rounded-bl-[4rem] flex flex-col space-y-4 items-center hover:scale-105 transition ease-in-out shadow-2xl">
         <div className="relative">
             <img src={Commercial} alt="commercial properties" className="max-w-lg h-96 rounded-tr-[4rem] rounded-bl-[4rem]" />
             <p className="absolute bottom-4 bg-white p-4 rounded-xl right-2">{numProperties?.commercial} properties</p>
@@ -36,7 +36,7 @@ function PropertyTypes() {
         <h4 className="text-white text-xl">Commercial</h4>
       </Link>
 
-      <Link className="bg-slate-800 p-6 rounded-bl-[4rem] rounded-tr-[4rem] flex flex-col space-y-4 items-center hover:scale-105 transition ease-in-out shadow-2xl">
+      <Link to={"/properties/all/property?type=residential"} className="bg-slate-800 p-6 rounded-bl-[4rem] rounded-tr-[4rem] flex flex-col space-y-4 items-center hover:scale-105 transition ease-in-out shadow-2xl">
         <div className="relative">
             <img src={Residential} alt="commercial properties" className="max-w-lg h-96 rounded-tr-[4rem] rounded-bl-[4rem]" />
             <p className="absolute bottom-4 bg-white p-4 rounded-xl right-2">{numProperties?.residential} properties</p>
